a8a99e5b1ca834ff78daec6ed962ee8912370994
thisisakash92 <thisisakash92@yahoo.com>
2020-08-28 10:38:25 +0530

    cleaned up model fields and migrations

--- crudsandthreads/models.py
+++ crudsandthreads/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=50)
-    price = models.FloatField(default=0.0)
     perfect_attendance = models.BooleanField(default=True)  # True if attended all classes.
 
 
 class Attendance(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    Date = models.DateField()
+    date = models.DateField()
     present = models.BooleanField(default=False)
