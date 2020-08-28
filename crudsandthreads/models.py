a3e4d0cf919fc21024401e06ebff8e87f2566400
thisisakash92 <thisisakash92@yahoo.com>
2020-08-28 11:15:28 +0530

    perfect attendace of student now automatically updates on each new attendance object

--- crudsandthreads/models.py
+++ crudsandthreads/models.py
@@ -10,3 +10,10 @@ class Attendance(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     date = models.DateField()
     present = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.present:
+            student = Student.objects.get(id=self.student.id)
+            student.perfect_attendance = False
+            student.save()
+        super(Attendance, self).save(*args, **kwargs)
