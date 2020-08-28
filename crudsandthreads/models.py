39744a95ab30b0255bec0a2be06e473d3a7001b4
thisisakash92 <thisisakash92@yahoo.com>
2020-08-28 10:19:18 +0530

    created models

--- crudsandthreads/models.py
+++ crudsandthreads/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField(default=0.0)
+    perfect_attendance = models.BooleanField(default=True)  # True if attended all classes.
+
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student)
+    Date = models.DateField()
+    present = models.BooleanField(default=False)
